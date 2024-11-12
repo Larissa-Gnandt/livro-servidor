b65c991a720142269e7942fd33184bb0ed95b0a8
Larissa <larissagnandt@gmail.com>
2024-11-12 15:20:42 -0300

    Configuração do servidor express e genrenciamento do sistema de CORS

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
+var cors = require('cors');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const livrosRouter = require('./routes/livros');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-const livrosRouter = require('./routes/livros');
-app.use('/livros', livrosRouter);
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +23,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', indexRouter); //Rota raiz
 app.use('/users', usersRouter);
+app.use('/livros', livrosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
