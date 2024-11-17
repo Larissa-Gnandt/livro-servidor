03ee9af450a5603a4134c832442bc920c03a3c1a
Larissa <larissagnandt@gmail.com>
2024-11-17 13:03:39 -0300

    Ajuestes no ControleLivros.ts

--- modelo/livro-dao.js
+++ modelo/livro-dao.js
@@ -3,7 +3,12 @@ const Livro = require("./livro-schema");
 const obterLivros = async () => {
   try {
     const livros = await Livro.find();
-    return livros;
+
+    const livrosFormatados = livros.map(livro => {
+      livro.codigo = livro._id.toString();  
+      return livro;
+    });
+    return livrosFormatados;
   } catch (error) {
     throw new Error(`Erro ao obter livros: ${error.message}`);
   }
@@ -11,12 +16,16 @@ const obterLivros = async () => {
 
 const incluir = async (livro) => {
   try {
-    const novoLivro = await Livro.create(livro);
+    const novoLivro = await Livro.create({
+      ...livro,  
+      codigo: livro.codigo.toString() 
+    });
     return novoLivro;
   } catch (error) {
     throw new Error(`Erro ao incluir livro: ${error.message}`);
   }
 };
+
 
 const excluir = async (codigo) => {
   try {
