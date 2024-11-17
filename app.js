f1e66dc70089ddb6cc30978ad97fec214bc00bef
Larissa <larissagnandt@gmail.com>
2024-11-17 12:23:02 -0300

    Remoção de comentários em app.js e correção do nome da const livroRouter

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const livrosRouter = require('./routes/livros');
+const livroRouter = require('./routes/livros');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -13,7 +13,6 @@ var app = express();
 
 app.use(cors());
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -23,22 +22,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter); //Rota raiz
+app.use('/', indexRouter); 
 app.use('/users', usersRouter);
-app.use('/livros', livrosRouter);
+app.use('/livros', livroRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+ 
   res.status(err.status || 500);
   res.render('error');
 });
